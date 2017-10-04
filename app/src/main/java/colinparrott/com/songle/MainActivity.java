010f3676f8e469ffce382730310917acdc4ea8f7
Colin <colinparrott2@gmail.com>
2017-10-04 17:37:53 +0100

    - Added basic permissions - Added Google Play dependencies - Created basic menu layout

--- app/src/main/java/colinparrott/com/songle/MainActivity.java
+++ app/src/main/java/colinparrott/com/songle/MainActivity.java
@@ -2,12 +2,32 @@ package colinparrott.com.songle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button playButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get play button
+        playButton = (Button) findViewById(R.id.btn_Play);
+
+        // Handle play button click
+        playButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.d("Menu", "Play button clicked");
+            }
+        });
     }
 }
