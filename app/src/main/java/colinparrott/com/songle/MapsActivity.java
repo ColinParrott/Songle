901253e59c7602e726172441ac599fa70ced121e
Colin <colinparrott2@gmail.com>
2017-10-17 22:11:35 +0100

    - Added guess button on map with pop up prompt - Added custom cursor color style

--- app/src/main/java/colinparrott/com/songle/MapsActivity.java
+++ app/src/main/java/colinparrott/com/songle/MapsActivity.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
